909c3ecbf48d70521fa084bfac0ceb3309844892
Andrew Kramer <1988kramer@gmail.com>
2016-04-20 19:01:40 -0700

    matrixRotate.cpp works now!

--- matrixRotate.cpp
+++ matrixRotate.cpp
@@ -19,30 +19,32 @@ int **matrix;
 // and the highest index in the matrix, NOT the size
 void rotateLeftHelper(int row, int column, int n) 
 {
-	int temp = matrix[row][column];
-	for (int i = 0; i < 3; i++) 
-	{
-		int nextRow = column;
-		int nextColumn = n - row;
-		matrix[row][column] = matrix[nextRow][nextColumn];
-		row = nextRow;
-		column = nextColumn;
-	}
-	matrix[row][column] = temp;
+  int temp = matrix[row][column];
+  cout << "temp: " << endl;
+  for (int i = 0; i < 3; i++) 
+    {
+      int nextRow = column;
+      int nextColumn = n - row;
+      matrix[row][column] = matrix[nextRow][nextColumn];
+      row = nextRow;
+      column = nextColumn;
+    }
+  matrix[row][column] = temp;
 }
 
 // accepts a pointer to a square two dimensional array 
 // and an int, the matrix size
 void rotateLeft(int n) 
 {
-	n -= 1; // switch n from matrix size to highest index
-	for (int i = 0; i < n/2; i++) 
-	{
-		for (int j = 0; j < n; j++) 
-		{
-		  rotateLeftHelper(i, j, n);
-		}
-	}
+  cout << "Rotating!" << endl;
+  n -= 1; // switch n from matrix size to highest index
+  for (int i = 0; i <= n/2; i++) 
+  {
+    for (int j = 0; j < n; j++) 
+    {
+      rotateLeftHelper(i, j, n);
+    }
+  }
 }
 
 // accepts a string designating the name of a file
@@ -51,25 +53,25 @@ void readFile(string fileName, int &n)
 {
 	const char* name = fileName.c_str();
 	ifstream input(name);
-	input >> n;
-	cout << n << endl;
-	matrix = new int*[n];
 	if (!input) 
 	{
 		cerr << "file: " << fileName << " does not exist";
 		exit(1);
 	}
 	cout << "file found" << endl;
+	string line;
+	getline(input, line);
+	istringstream iss(line);
+	iss >> n;
+	cout << n << endl;
+	matrix = new int*[n];
 	for (int i = 0; i < n; i++) 
 	{
 		matrix[i] = new int[n];
 	}
 	for (int i = 0; i < n; i++) 
 	{
-		string line;
-		cout << line << endl;
 		getline(input, line);
-		cout << line << endl;
 		istringstream iss(line);
 		for (int j = 0; j < n; j++) 
 		{
@@ -80,34 +82,34 @@ void readFile(string fileName, int &n)
 
 void printMatrix(int n) 
 {
-	for (int i = 0; i < n; i++) 
+  for (int i = 0; i < n; i++) 
+    {
+      cout << "{" << matrix[i][0];
+      for (int j = 1; j < n; j++) 
 	{
-		cout << "{" << matrix[i][0];
-		for (int j = 1; j < n; j++) 
-		{
-			cout << ", " << matrix[i][j];
-		}
-		cout << "}" << endl;
+	  cout << ", " << matrix[i][j];
 	}
-	cout << endl;
+      cout << "}" << endl;
+    }
+  cout << endl;
 }
 
 void deleteMatrix(int n) 
 {
-	for (int i = 0; i < n; i++)
-	{
-		delete[] matrix[i];
-	}
-	delete[] matrix;
+  for (int i = 0; i < n; i++)
+    {
+      delete[] matrix[i];
+    }
+  delete[] matrix;
 }
 
 int main(int argc, char* argv[]) 
 {
-	int n = 0;
-	readFile(argv[1], n);
-	printMatrix(n);
-	rotateLeft(n);
-	printMatrix(n);
-	deleteMatrix(n);
-	return 0;
+  int n = 0;
+  readFile(argv[1], n);
+  printMatrix(n);
+  rotateLeft(n);
+  printMatrix(n);
+  deleteMatrix(n);
+  return 0;
 }
